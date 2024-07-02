0c26b328e1000a919e3a3bdaf46c2281c5be8449
Raj Yadav <rajyadav751811@gmail.com>
2024-07-02 15:41:00 +0530

    Spring Schedular Application

---
+++ 01_Spring_Schedular/src/main/java/in/raj/service/ReportService.java
@@ -0,0 +1,12 @@
+package in.raj.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+    @Scheduled(fixedRate = 3000)
+    public void generateReport() {
+        System.out.println("Report generated");
+    }
+}
